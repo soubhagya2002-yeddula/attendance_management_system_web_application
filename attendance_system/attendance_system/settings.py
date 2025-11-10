3f468e1d50cc23a6504f4b42dcfa7b8165bafc0a
Y Soubhagya <soubhagyayeddula2002@gmail.com>
2025-11-10 12:42:16 +0530

    Added Procfile, requirements.txt, and updated settings.py for Render deployment

--- attendance_system/attendance_system/settings.py
+++ attendance_system/attendance_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@#_7-u#(v0f=@d10a&wy4*e45&8kygk7zn$&f6)0p3w3s)g#ae
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS',"*").SPLIT(",")]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR/'staticfiles'
+
+STATICFILES_STORAGE='whitenoise.storage.CompressManifestStaticFileStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
